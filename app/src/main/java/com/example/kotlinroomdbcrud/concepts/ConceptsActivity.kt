06ad429c788214791b470a3de3461fb87cc4d6b8
RahilRK <rahilmithani007@gmail.com>
2022-04-07 22:08:29 +0530

    #2 commit

--- app/src/main/java/com/example/kotlinroomdbcrud/concepts/ConceptsActivity.kt
+++ app/src/main/java/com/example/kotlinroomdbcrud/concepts/ConceptsActivity.kt
@@ -1,8 +1,9 @@
-package com.example.kotlinroomdbcrud
+package com.example.kotlinroomdbcrud.concepts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.example.kotlinroomdbcrud.R
 
 class ConceptsActivity : AppCompatActivity() {
 
@@ -20,7 +21,29 @@ class ConceptsActivity : AppCompatActivity() {
 //        scopeWith()
 //        scopeApply()
 //        scopeAlso()
-        lazyEg()
+//        lazyEg()
+
+        /*val add = add()
+        add.doSum(2,3)
+        add.doSum(2,3, object: helpToSum {
+            override fun doMySum(sum: Int) {
+                Log.e("add", "doMySum:- $sum")
+            }
+        })*/
+
+        /*val human = Human()
+        human.age = 50
+        Log.e("human", "age:- ${human.age}")*/
+
+        /*val student = Student()
+        student.addHobby("Game")
+        student.addHobby("TV")
+        Log.e("student", "hobby:- ${student.hobby}")*/
+
+        /*var successMsg =  ApiResult.success("data got")
+        var failMsg =  ApiResult.fail("null")
+        var loadingMsg =  ApiResult.loading("isLoading")
+        sealedClassEg(loadingMsg)*/
     }
 
     fun equalToLogic() {
@@ -86,6 +109,35 @@ class ConceptsActivity : AppCompatActivity() {
     fun lazyEg() {
         Log.e(TAG, "lazyEg:- $name")
     }
+
+    fun sealedClassEg(result: ApiResult) {
+
+        when (result) {
+            is ApiResult.success -> Log.e("sealedClassEg","${result.data}")
+            is ApiResult.fail -> Log.e("sealedClassEg","${result.message}")
+            is ApiResult.loading -> Log.e("sealedClassEg","${result.message}")
+            else -> Log.e("sealedClassEg","else")
+        }
+    }
+}
+
+class add {
+
+    fun doSum(num1: Int, num2: Int) {
+
+        val sum = num1 + num2
+        Log.e("add", "sum:- $sum")
+    }
+
+    fun doSum(num1: Int, num2: Int, helpToSum: helpToSum) {
+
+        val sum = num1 + num2
+        helpToSum.doMySum(sum)
+    }
+}
+
+interface helpToSum {
+    fun doMySum(sum: Int)
 }
 
 class Person(a: String)
@@ -95,4 +147,34 @@ class MySelf {
     var name: String = ""
 //    var age: Int = 28
     var age: Int = 0
+}
+
+//todo eg#1 of Backing property/field
+class Human {
+    private var _age: Int = 0
+    var age: Int
+        get() {
+            return _age
+        }
+        set(value) {
+            _age = value
+        }
+
+}
+
+//todo eg#2 of Backing property/field
+class Student {
+    private var _hobby = mutableListOf<String>()
+    val hobby: List<String>
+    get() = _hobby
+
+    fun addHobby(text: String) {
+        _hobby.add(text)
+    }
+}
+
+sealed class ApiResult() {
+    class loading(var message: String): ApiResult()
+    class success(var data: String): ApiResult()
+    class fail(var message: String): ApiResult()
 }
